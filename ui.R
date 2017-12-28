2df492995b1e9964380276fcad21c790deabd61e
dimitribessa <dimitribessa@gmail.com>
2017-12-28 21:01:09 -0200

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,34 @@
+#mapa versao 0.1
+
+library('shiny')
+library('plotly')
+library('dplyr')
+library('reshape2')
+library('stringr')
+#setwd('D:\\Dimitri\\Docs 17-out-15\\Ju html\\Treino OnLine')
+#treino <- read.xlsx('08nov2017.xlsx', sheetIndex = 3, encoding = 'UTF-8')
+load('data.RData')
+
+# Use a fluid Bootstrap layout
+shinyUI ( fluidPage(    
+  
+  # Give the page a title
+  fluidRow(
+    column(2,tags$img(height = 100,
+                    width = 100,
+                    src = "JB_quad.jpg")),
+    column(3,titlePanel('Selecione o treino'))
+    ),
+  fluidRow(
+       column(4,
+      selectInput("treino", "Treino:", 
+                  choices= unique(treino$Treino), selected=unique(treino$Treino)[1]),
+      hr(),
+      helpText("Período: 27 de novembro a 8 de dezembro de 2017.")
+    ),
+    
+      #Tabela com as informações
+      fluidRow(
+        DT::dataTableOutput("table"))   
+    ))
+)
