d9934057e6ad031e8523522f588c42d8ce132e80
dimitribessa <dimitribessa@gmail.com>
2018-01-17 01:47:22 -0200

    Update server.R

--- matriz/server.R
+++ matriz/server.R
@@ -8,45 +8,30 @@ library('intergraph')
 library('ggnetwork')
 library('ggplot2')
 library('network')
-
-load('rdata.RData')
+library('networkD3')
 #options(encoding = 'UTF-8')
 
 shinyServer(function(input, output, session) {
-  ds3 <- reactive({if(input$checkbox){
-      Li <- matriz(input$input, input$output, input$num)}else{
-      Li    <- L}})
-  #funcao reactiva para o 
-  ds  <- reactive({set.seed(1234) 
-    Lii <- ds3()  
-    Lmelt  <- melt(Lii, id.vars = c('Setor')) 
-    lteste <- Lmelt[Lmelt$value >= input$range[1] & Lmelt$value <= input$range[2]  ,] %>%                                                   graph_from_data_frame(., vertices = Setor) %>% 
-      simplify(., remove.multiple = F, remove.loops = T)  %>% fortify(.,weights = 'value',arrow.gap = 0.01)
-    
-    #corrigindo o BUG          
-    lteste$x <- lteste$x[,1]
-    lteste$y <- lteste$y[,1]
-    lteste$xend <- lteste$xend[,1]
-    lteste$yend <- lteste$yend[,1]
-    lteste}
-  )
+    ds3 <- reactive({if(input$checkbox){
+                  BO <- BO
+                  Li <- matriz(input$input, input$output, input$num)}else{
+                  Li    <- L}})
+       #funcao reactiva para o 
+    ds  <- reactive({ 
+                  Lii <- ds3()  
+                  Lmelt  <- melt(Lii, id.vars = c('Setor')) 
+                  lteste <- Lmelt[Lmelt$value >= input$range[1] & Lmelt$value <= input$range[2]  ,] %>%                                                   graph_from_data_frame(., vertices = Setor) %>% 
+                  simplify(., remove.multiple = F, remove.loops = T) %>% 
+                   igraph_to_networkD3(., group = grupo)
+                   lteste
+                  })
   
-  output$rede <- renderPlot({
+  output$rede <- renderForceNetwork({
     ds2 <- ds()
-    grafico <-  ggplot(ds2, aes(x = x,  y = y, xend = xend, yend = yend)) +
-      geom_edges(aes(colour = Segmento),arrow = arrow(length = unit(3, "pt"), type = "closed"), curvature = 0.1, alpha = .5)  +
-      geom_nodes(aes(colour = Segmento),size = 3, alpha = .9) +
-      geom_nodetext_repel(aes(label =  vertex.names ), color = 'white') +
-      theme(axis.text = element_blank(),
-            axis.title = element_blank(),
-            panel.background = element_rect(fill = "black"),
-            plot.background = element_rect(fill = "black"),
-            legend.background = element_rect(fill="black", colour = 'black'),
-            legend.key = element_rect(fill = "black", colour = 'black'),
-            legend.position = 'bottom',
-            legend.text = element_text(size = 8, colour = "white"),
-            panel.grid = element_blank())
-    print(grafico)       
+    forceNetwork(Links = ds2$links, Nodes = ds2$nodes,
+    Source = 'source', Target = 'target', NodeID = 'name',
+    Group = 'group', zoom = T, legend = T, arrows = F, charge = -80,
+    fontSize = 10,opacityNoHover = 1.5)
   }) 
   output$table  <- DT::renderDataTable(DT::datatable({ds3()}))
 })
